b8c8d21a3d43cf4835ca7d3c9ef3c96bc78366f7
Robin Maben <robinmaben@users.noreply.github.com>
2016-11-23 17:10:54 +0530

    use default import

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
 require('babel-polyfill');
 
 var _draftJsPluginsEditor = require('draft-js-plugins-editor');
 
-exports['default'] = _draftJsPluginsEditor.Editor;
+var _draftJsPluginsEditor2 = _interopRequireDefault(_draftJsPluginsEditor);
+
+exports['default'] = _draftJsPluginsEditor2['default'];
 module.exports = exports['default'];
