de99e5878b51defadab3d01d11c94525f1e316dd
Robin Maben <robinmaben@users.noreply.github.com>
2016-11-23 16:57:38 +0530

    exported draft-js-plugins-editor and polyfill

---
+++ index.js
@@ -0,0 +1,4 @@
+import 'babel-polyfill';
+import {Editor} from 'draft-js-plugins-editor';
+
+export default Editor;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+require('babel-polyfill');
+
+var _draftJsPluginsEditor = require('draft-js-plugins-editor');
+
+exports['default'] = _draftJsPluginsEditor.Editor;
+module.exports = exports['default'];
